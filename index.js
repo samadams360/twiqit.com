f25ff49d56680fb66506ba8e9bfc088fb74aa8a0
Sam S <sam.theduke@gmail.com>
2026-03-12 09:47:58 -0700

    Update index.js for Railway

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-app.listen(port, () => {
+app.listen(port, "0.0.0.0",() => {
   console.log(`Server running on port ${port}`);
 });
